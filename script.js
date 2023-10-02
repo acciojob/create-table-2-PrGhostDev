51abae299ae50ed2e1421b20151002c13dc96f7a
PrGhostDev <139947007+PrGhostDev@users.noreply.github.com>
2023-10-02 18:45:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function createTable() {
     //Write your code here
-  
+    const numRows = prompt('Input number of rows:');
+    
+    const numCols = prompt('Input number of columns:');
+    
+    const table = document.getElementById('myTable');
+    
+    table.innerHTML = '';
+    for (let i = 0; i < numRows; i++) {
+        const row = table.insertRow(i);
+
+        for (let j = 0; j < numCols; j++) {
+            const cell = row.insertCell(j);
+            cell.textContent = `Row-${i} Column-${j}`;
+        }
+    }  
 }
